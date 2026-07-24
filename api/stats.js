fd4208dd4d7844f2809322f09fd71f821ad766d4
VoltexHub <webingwme@gmail.com>
2026-07-24 03:26:15 -0500

    api change incase discord fails

--- api/stats.js
+++ api/stats.js
@@ -5,6 +5,16 @@ export default async function handler() {
         `https://discord.com/api/v10/guilds/${guild_id}/widget.json?with_counts=true`
     );
 
+    if (!response.ok) {
+        // widget likely disabled in Discord server settings, or bad guild id
+        return Response.json({
+            total_executions: 0,
+            active_users: 0,
+            api_status: "Operational",
+            discord_community: {}
+        });
+    }
+
     const discord_community = await response.json();
 
     return Response.json({

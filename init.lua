39902ed5c02733f1c16e419b6e2af8265099de20
matpat <webingwme@gmail.com>
2026-07-25 19:57:22 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,6 @@
-local GameLoader = loadstring(game:HttpGet("https://roblox-alpha-murex.vercel.app/src/games/loader.lua"))()
-local ValidGame = GameLoader.IsValid
+local Script = shared.script
+	-- "scripts/ascii"
+	-- "games/catastrophia"
 
-if ValidGame then
-	GameLoader.LoadScript()
-end
-
-local ScriptLoader = loadstring(game:HttpGet("https://roblox-alpha-murex.vercel.app/src/scripts/loader.lua"))()
-
-if ScriptLoader.IsValid and not ValidGame then
-	ScriptLoader.LoadScript()
-else
-	GameLoader.Error()
-end
-
--- loadstring(game:HttpGet("https://roblox-alpha-murex.vercel.app/init.lua"))()
+local Loader = loadstring(game:HttpGet(string.format("https://roblox-alpha-murex.vercel.app/src/%s/main.lua", Script)))
+Loader()

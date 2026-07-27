54f0acaeb7086548711c1cc67fef956f4bc3fdf4
matpat <webingwme@gmail.com>
2026-07-26 20:59:37 -0500

    Toggle autowin and infmoney settings in main.lua

--- src/games/Horse-Race/main.lua
+++ src/games/Horse-Race/main.lua
@@ -1,15 +1,16 @@
-getgenv().autowin = getgenv().autowin or false
-getgenv().infmoney = getgenv().infmoney or true
+getgenv().autowin = getgenv().autowin or true
+getgenv().infmoney = getgenv().infmoney or false
 
-local path = workspace.Areas:FindFirstChildWhichIsA("Model")
+local Services = game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services
+local path = workspace.Areas:FindFirstChildWhichIsA("Folder")
 while getgenv().autowin do
     task.wait(0.6)
 
-    game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.FightService.RE.JoinContest:FireServer(path.Name)
+    Services.FightService.RE.JoinContest:FireServer(path.Name)
     for i, gate in ipairs(path.WinGates:GetChildren()) do
-        game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.FightService.RE.GetWinsEvent:FireServer("WinGate_" .. tostring(i), gate:FindFirstChild("gate").Position)
+        Services.FightService.RE.GetWinsEvent:FireServer("WinGate_" .. tostring(i), gate:FindFirstChild("gate").Position)
     end
-    game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.FightService.RE.QuitContestEvent:FireServer(path.Name)
+    Services.FightService.RE.QuitContestEvent:FireServer(path.Name)
 end
 
-if getgenv().infmoney then game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.5.1"].knit.Services.RandomPotionService.RE.BuyPotionEvent:FireServer(table.unpack({[1] = -1e64, [2] = {}, [3] = true, [4] = "yippie!", })) end
+if getgenv().infmoney then Services.RandomPotionService.RE.BuyPotionEvent:FireServer(table.unpack({[1] = -1e64, [2] = {}, [3] = true, [4] = "yippie!", })) end

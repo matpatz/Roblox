57a263316670167842b846f47a20e0b2a17069ad
matpat <webingwme@gmail.com>
2026-08-06 08:23:57 -0500

    Create Platform.lua

---
+++ src/Modules/Platform.lua
@@ -0,0 +1,20 @@
+local uis = cloneref(game:GetService("UserInputService"))
+
+local function device()
+	local platform = tostring(uis:GetPlatform()):split(".")[3]
+
+	if platform == "Windows" or platform == "OSX" or platform == "UWP" then
+		return "PC"
+	elseif platform == "Android" or platform == "IOS" then
+		return "Mobile"
+	else
+		return "Other"
+	end
+end
+
+getgenv().device = device()
+
+--[[
+	local device = getgenv().device or "Mobile"
+	print(device)
+--]]

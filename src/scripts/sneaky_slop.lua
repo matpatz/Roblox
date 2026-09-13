51dd55d3cbc7203ecfd8085cd3f0a49fe8aea39c
VoltexHub <webingwme@gmail.com>
2026-09-13 11:00:11 -0500

    hey goob goob

--- src/scripts/sneaky_slop.lua
+++ src/scripts/sneaky_slop.lua
@@ -1,21 +1,25 @@
+-- loadstring(game:HttpGet("https://www.voltex.website/src/scripts/sneaky_slop.lua"))()
 -- Im aware this could be detected, but I doubt you would do something like that.
 
-local OurBase = `https://roblox-alpha-murex.vercel.app/src/Modules/sneaky_slop`;
-local OurUrl = `{OurBase}/SilentAim.lua`;
+local OurBase = `https://roblox-alpha-murex.vercel.app/src/Modules/sneaky_slop`
+local SilentAim = `{OurBase}/SilentAim.lua`
 
--- only their UI has this marker; their request.luau also mentions their own domain
-local UIMarker = "HOLD TO DRAG";
+local OurSource = game:HttpGet(SilentAim);
 
-local Cached;
+local sneeky_fov_toggle = "sneeky_fov_toggle"
+local unrestricted_main = "NOTIFICATION_LIBRARY/unrestricted_main.luau"
+
+if isfunctionhooked(loadstring) then
+    restorefunction(loadstring)
+end
 
 local Old; Old = hookfunction(loadstring, newcclosure(function(...)
 	local Args = {...};
 	local Source = Args[1];
 
-	if type(Source) == "string" and Source:find(UIMarker, 1, true) then
-		Cached = Cached or game:HttpGet(OurUrl);
-		Args[1] = Cached;
-	end;
+	if type(Source) == "string" and Source:find(sneeky_fov_toggle, 1, true) and Source:find(unrestricted_main, 1, true) then
+		Args[1] = OurSource;
+	end
 
 	return Old(table.unpack(Args));
 end));

87088090f100203d27fee4a3e1246f98d09a1f74
VoltexHub <webingwme@gmail.com>
2026-09-13 17:22:58 -0500

    thhis

--- src/Modules/Knit/v1/cache.lua
+++ src/Modules/Knit/v1/cache.lua
@@ -32,6 +32,7 @@ cache.set = function(key, value)
     else
         cached["keys"][key] = nil -- clear cached key
     end
+    return cached["keys"][key]
 end
 
 cache.make_cache = function(cache_table, cache_time)

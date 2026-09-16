b5e5c5a0e6f1b9bb4ddebe1197afeba3f6a8c5ab
matpat <webingwme@gmail.com>
2026-09-16 11:43:48 -0500

    finish

--- src/scripts/MoreUnc-v4/Functions/instance/firetouchinterest.lua
+++ src/scripts/MoreUnc-v4/Functions/instance/firetouchinterest.lua
@@ -1,17 +1,10 @@
-AddFunction("firetouchinterest", function(Part, Target, Touch)
-    if not (typeof(Part) == "Instance" and Part:IsA("BasePart")) then error("expected BasePart at argument #1, got " .. typeof(Part), 2) end
-    if not (typeof(Target) == "Instance" and Target:IsA("BasePart")) then error("expected BasePart at argument #2, got " .. typeof(Target), 2) end
-    if Touch ~= nil and type(Touch) ~= "boolean" then error("expected boolean or nil at argument #3, got " .. type(Touch), 2) end
-    if Touch == false then
-        firesignal(Target.TouchEnded, Part)
-    else
-        firesignal(Target.Touched, Part)
-    end
-end)
-
-
 -- @param1 the object to touch that the touchinterest is connected to
 -- @param2 a player object (Hrp, Head, etc)
--- 
-return function(part, target, touch)
+-- @param3 a boolean to or to not touch
+return function(part: Instance, target: Instance, touch: boolean)
+    if touch then
+        firesignal(target.Touched, part)
+    else
+        firesignal(target.TouchEnded,part)
+    end
 end
